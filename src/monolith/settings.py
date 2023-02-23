98262200d3bba55d4d76d427a6ed91f5f85a4445
Joshua Crews <jacrews1@sheffield.ac.uk>
2023-02-23 15:02:00 +0000

    Moved python references to local monolithic design now

--- src/monolith/settings.py
+++ src/monolith/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'spare_food_share.urls'
+ROOT_URLCONF = 'monolith.urls'
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'spare_food_share.wsgi.application'
+WSGI_APPLICATION = 'monolith.wsgi.application'
 
 
 # Database
