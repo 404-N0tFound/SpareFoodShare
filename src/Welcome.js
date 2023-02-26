64a503cc279f2a36d770d2a715b25ceeb5edcddb
Joshua Crews <jacrews1@sheffield.ac.uk>
2023-02-26 19:48:12 +0000

    Started work on welcome page

---
+++ src/Welcome.js
@@ -0,0 +1,25 @@
+import "./Welcome.css";
+
+function Welcome() {
+    return (
+        <div className="Welcome">
+            <header className="Welcome-header">
+                <h1>Spare Food Share</h1>
+                <p>Login |</p>
+                <p>About Us |</p>
+                <p>Browse |</p>
+                <p>Contact</p>
+            </header>
+            <body className="Welcome-body">
+                <h1>
+                    Our mission is to facilitate food sharing within the community, battling food waste and insecurity.
+                </h1>
+                <p>
+                    It is estimated that the UK&apos;s total food waste could feed upwards of 30 million people in a year. Be part of the solution, not the problem - share your spare groceries with people in your area!
+                </p>
+            </body>
+        </div>
+    );
+}
+
+export default Welcome;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {} from "./index.css";
-import App from "./App";
+import Welcome from "./Welcome";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
         <React.StrictMode>
-            <App />
+            <Welcome />
         </React.StrictMode>
 );
 
