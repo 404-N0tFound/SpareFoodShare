7e69d223ab335779298364638df3cec24102fadb
Joshua Crews <jacrews1@sheffield.ac.uk>
2023-02-26 20:14:37 +0000

    Add some inputs that will become buttons on the landing page

--- src/Welcome.js
+++ src/Welcome.js
@@ -17,6 +17,8 @@ function Welcome() {
                 <p>
                     It is estimated that the UK&apos;s total food waste could feed upwards of 30 million people in a year. Be part of the solution, not the problem - share your spare groceries with people in your area!
                 </p>
+                <input className="loginButton" type="button" value="Login"/>
+                <input className="browseButton" type="button" value="Browse"/>
             </body>
         </div>
     );
