5654f8eea683acb51594692d65c993f02c7bbbc5
ziyanliu <ZLiu208@sheffield.ac.uk>
2023-02-28 18:52:58 +0000

    Update Date: 28/02/2023 18:52 pm

--- src/monolith/urls.py
+++ src/monolith/urls.py
@@ -19,6 +19,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('sparefood.urls')),
-    # path('', views.index, name='index'),
-    # path('listings/', views.listings, name='listings')
 ]
