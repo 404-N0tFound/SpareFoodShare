53904281d5e2238102272bdae2f30fad41fb6df7
Joshua Crews <jacrews1@sheffield.ac.uk>
2023-03-01 21:17:08 +0000

    Moved the buttons on the homepage to look more pleasing and changing the listings to use the current navbar

--- src/jsx/components/Navbar.js
+++ src/jsx/components/Navbar.js
@@ -4,16 +4,18 @@ import "./Navbar.css";
 class Navbar extends Component {
     render() {
         return (
-                <div className="Navbar-header">
+                <div className="Welcome-header">
                     <div className="Website Logo">
-                        <a href="../">
+                        <a href="./">
                             <h1>Spare Food Share</h1>
                         </a>
                     </div>
                     <div className="links-list">
                         <ul>
-                            <a href="./">Home</a>
-                            <a href="./browse">Profile</a>
+                            <a href="./listings">Login(listings)</a>
+                            <a>About Us</a>
+                            <a href="./browse">Browse</a>
+                            <a className="contact">Contact</a>
                         </ul>
                     </div>
                 </div>
