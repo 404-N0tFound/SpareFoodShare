143683afed5954b66c5bf9dee613242931e2cde4
Ziyan Liu <lzy943884014@gmail.com>
2023-03-07 22:09:14 +0000

    Update to Version 0.1.5 - Date:07/03/2023 22:07pm - Fix the dynamic img display bug (in a very strange way), and constraints when item not available.

--- src/jsx/item/Item.js
+++ src/jsx/item/Item.js
@@ -7,7 +7,7 @@ import { useState, useEffect } from 'react';
 function Item(){
     const { item_id } : { item_id : string } = useParams();
     const [item, setItem] = useState([]);
-    const [isLoaded, setIsLoaded] = useState(true);
+    const [isLoaded, setIsLoaded] = useState(false);
 
     useEffect(() => {
         fetch('../api/items/'+item_id+'/')
@@ -19,14 +19,16 @@ function Item(){
 
 
     if(isLoaded){
+        // if(isAvailable) // check if item is expired or deleted or becomes private, if so, show "page not available", and redirect to Browse page
         console.log(item.item_pic);
         console.log(isLoaded);
+        const img_src = item.item_pic;
         return(
             <div className="page-content">
                 <Navbar />
                 <body className="item-body">
                         <div className="item-content">
-                            <img className="item-pic" src={ src } />
+                            <img className="item-pic" src={ require(`../pics/${img_src}`) } />
                             <div className="item-vl"></div>
                             <div className="item_info">
                                 <h3>Name: { item.item_name }</h3>
