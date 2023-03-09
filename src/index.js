8a0a3a643f9684a35275ac30b5676e9d5c997fa4
ariinabulantseva <ariinabulantseva@gmail.com>
2023-03-09 16:22:31 +0000

    an almost working login page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,39 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {} from "./index.css";
-import App from "./App";
+import Layout from "./jsx/Layout";
+import Welcome from "./jsx/welcome/Welcome";
+import Browse from "./jsx/browse/Browse";
+import Login from "./jsx/login/Login";
 import reportWebVitals from "./reportWebVitals";
+import Item from "./jsx/item/Item";
+import Upload from "./jsx/upload/Upload";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-);
+export default function App() {
+    return (
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Welcome />} />
+                        <Route path="browse" element={<Browse />} />
+                        <Route path="login" element={<Login />} />
+                        <Route path="item/:item_id" element={<Item />} />
+                        <Route path="upload" element={<Upload />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
+        );
+}
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
---
+++ src/jsx/components/Navbar.js
@@ -0,0 +1,26 @@
+import { Component } from "react";
+import "./Navbar.css";
+
+class Navbar extends Component {
+    render() {
+        return (
+        <div className="navbar">
+                <div className="header">
+                    <div className="website-logo">
+                        <a href='../'>
+                            <h1 className="nav-header">Spare Food Share</h1>
+                        </a>
+                    </div>
+                    <div className="links-list">
+                        <ul>
+                            <a href="./login" className="login-a">Login</a>
+                            <a href="./browse" className="browse-a">Browse</a>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            );
+    }
+}
+
+export default Navbar;
