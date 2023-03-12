461ea829b8be632822f67889c0d77c969ef3f45d
Joshua Crews <jacrews1@sheffield.ac.uk>
2023-03-12 12:05:28 +0000

    Implemented logout functionality and token refreshment with a status 400 error on instancing

--- src/jsx/MyProfile/MyProfile.js
+++ src/jsx/MyProfile/MyProfile.js
@@ -1,11 +1,18 @@
 import "./MyProfile.css";
 import "../components/Theme.css";
 import ProfileFramework from "../components/ProfileFramework";
+import { useContext } from "react";
+import AuthContext from "../AuthContext";
 
 function MyProfile() {
+    let {user} = useContext(AuthContext)
     return(
         <div className="my-profile">
             <ProfileFramework />
+            <p>
+                Master Windu, <b><i>you survived...</i></b>
+            </p>
+            {user && <p>It&apos;s over {user.username}, I have the high ground!</p>}
         </div>
     );
 }
--- src/jsx/components/ProfileFramework.js
+++ src/jsx/components/ProfileFramework.js
@@ -1,36 +1,37 @@
-import { Component } from "react";
+import {useContext} from "react";
 import "./ProfileFramework.css";
 import Navbar from "./Navbar";
+import AuthContext from "../AuthContext";
 
-class ProfileFramework extends Component {
-    render() {
-        return (
-            <div className="page-content">
-                <Navbar />
-                <div className="sidebar">
-                    <div>
-                        <a href='../profile'>
-                            My Profile
-                        </a>
-                    </div>
-                    <div>
-                        <a>
-                            Upload
-                        </a>
-                    </div>
-                    <div>
-                        <a>
-                            Logout
-                        </a>
-                    </div>
+function ProfileFramework () {
+    let {logoutUser} = useContext(AuthContext)
+
+    return (
+        <div className="page-content">
+            <Navbar />
+            <div className="sidebar">
+                <div>
+                    <a href='../profile'>
+                        My Profile
+                    </a>
                 </div>
-
-                <div className="page-value">
-                    
+                <div>
+                    <a>
+                        Upload
+                    </a>
+                </div>
+                <div>
+                    <a onClick={logoutUser} href={'../login'}>
+                        Logout
+                    </a>
                 </div>
             </div>
-        );
-    }
+
+            <div className="page-value">
+
+            </div>
+        </div>
+    );
 }
 
 export default ProfileFramework;
