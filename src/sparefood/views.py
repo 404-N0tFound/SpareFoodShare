c8814cf797f92963ce7d0883b77e239c74c4907a
Ziyan Liu <lzy943884014@gmail.com>
2023-03-20 17:16:58 +0000

    Update to Version 0.1.9 Date:20/03/2023 17:12 - Remove the import of crsf_exempt

--- src/sparefood/views.py
+++ src/sparefood/views.py
@@ -1,5 +1,4 @@
 from django.http import JsonResponse, HttpResponse, HttpResponseRedirect
-from django.views.decorators.csrf import csrf_exempt
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
