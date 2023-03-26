97f9ea77ee24941b4d73f79bc0673f3299c6e11a
Sachin Mathur <sachinmathur@Sachins-MacBook-Air.local>
2023-03-26 00:15:06 +0000

    Made Browse Page Popup

--- src/jsx/browse/Browse.js
+++ src/jsx/browse/Browse.js
@@ -1,83 +1,73 @@
-import { useState, useEffect } from 'react';
+/* eslint-disable */
 import "./Browse.css";
 import "../components/Theme.css";
 import Navbar from "../components/Navbar";
-import pic from "../pics/test.jpg"
-import {useNavigate} from "react-router-dom";
 import carrot from "../pics/carrot.svg";
 import apple from "../pics/apple.svg";
 import mushroom from "../pics/mushroom.svg";
 
-function Browse(){
-  const [items, setItems] = useState([]);
-  const fetchData = () => {
-    return fetch('../api/items/')
-          .then((response) => response.json())
-          .then((data) => setItems(data))
-  }
-  useEffect(() => {
-    fetchData()
-  },[])
-
-  let navigate = useNavigate();
-
-  const btn_clicked = (id) => {
-    alert('You clicked ' + id);
-    let path = '../item/' + id;
-    navigate(path);
-}
+function Browse() {
     return (
         <div className="page-content">
-            <Navbar />
+            <Navbar/>
             <body className="listings-body">
-                <div className="listings-content">
-                    <ul>
-                        <div className="item-card">
-                            <li>
-                                <img className="items-pic" src={carrot} />
-                                    <div className="item_info">
-                                        <h3>Name</h3>
-                                        <p>Descriptions</p>
-                                    </div>
-                                    <button className="item_btn" onClick={() => btn_clicked(1)}>Details</button>
-                            </li>
-                        </div>
-
-                        <div className="item-card">
-                            <li>
-                                <img className="items-pic" src={apple} />
-                                    <div className="item_info">
-                                        <h3>Name</h3>
-                                        <p>Descriptions</p>
-                                    </div>
-                                    <button className="item_btn" onClick={() => btn_clicked(2)}>Details</button>
-                            </li>
-                        </div>
-                        <div className="item-card">
-                            <li>
-                                <img className="items-pic" src={mushroom} />
-                                    <div className="item_info">
-                                        <h3>Name</h3>
-                                        <p>Descriptions</p>
-                                    </div>
-                                    <button className="item_btn" onClick={() => btn_clicked(3)}>Details</button>
-                            </li>
-                        </div>
-                        {items && items.length > 0 && items.map((itemsObj, index) => (
-                            <li key={index}>
-                                <img className="items-pic" src={pic} />
-                                    <div className="item_info">
-                                        <h3>Name: {itemsObj.id}</h3>
-                                        <p>Des: {itemsObj.item_name}</p>
-                                    </div>
-                                    <button className="item_btn" onClick={() => btn_clicked( itemsObj.id )}>Details</button>
-                                </li>
-                            ))}
-                    </ul>
+                <a className="item-card" id="myBtn" onClick={openModal}>
+                    <img className="items-pic" src={carrot} alt="carrot"/>
+                    <h1>Carrot</h1>
+                    <p>Expiry Date</p>
+                    <p><button>Register Interest</button></p>
+                </a>
+                <a className="item-card" id="myBtn" onClick={openModal}>
+                    <img className="items-pic" src={apple} alt="apple"/>
+                    <h1>Apple</h1>
+                    <p>Expiry Date</p>
+                    <p><button>Register Interest</button></p>
+                </a>
+                <a className="item-card" id="myBtn" onClick={openModal}>
+                    <img className="items-pic" src={mushroom} alt="mushroom"/>
+                    <h1>Mushroom</h1>
+                    <p>Expiry Date</p>
+                    <p><button>Register Interest</button></p>
+                </a>
+                <div className="modal" id="myModal">
+                    <div className="modal-content">
+                        <span onClick={closeModal} className="close">&times;</span>
+                        <img className="items-pic" src={carrot} alt="carrot"/>
+                        <h1>Carrot</h1>
+                        <p>Description: </p>
+                        <p>Location: </p>
+                        <p>Expiry Date: </p>
+                        <p><button>Register Interest</button></p>
+                    </div>
                 </div>
             </body>
         </div>
     );
 }
 
+// Get the modal
+const modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+const btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+const openModal = () => {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+const closeModal = () => {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
 export default Browse;
