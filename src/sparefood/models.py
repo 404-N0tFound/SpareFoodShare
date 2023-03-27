2d7c1eeec08f9b5624e8841a470b78a8e35d6f69
Ziyan Liu <lzy943884014@gmail.com>
2023-03-27 05:33:39 +0100

    Update to Version 0.1.9 Date:27/03/2023 05:33 - Renamed field 'provider_id' to 'provider'(Or the field in database will be named 'provider_id_id');

--- src/sparefood/models.py
+++ src/sparefood/models.py
@@ -71,8 +71,8 @@ class Item(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField("name", max_length=240)
     description = models.TextField("description", max_length=10000)
-    provider_id = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id',
-                                    default="8ba97d61-4c1b-4d43-bcea-da0bd3653a7a")
+    provider = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id',
+                                 default="8ba97d61-4c1b-4d43-bcea-da0bd3653a7a")
     upload_date = models.DateField(default=timezone.now)
     expiration_date = models.DateField()
     status = models.CharField("status", max_length=30, default="Available")
@@ -89,7 +89,6 @@ class Item(models.Model):
 
 
 class Order(models.Model):
-
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     initiator = models.ForeignKey(User, on_delete=models.CASCADE, to_field='id')
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
