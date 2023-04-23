4118d74bc28416549f818a0d3db5a0786f606373
elp20xt <xtan30@sheffield.ac.uk>
2023-04-23 14:20:48 +0100

    solve conflicts between business and individual version in my code

--- src/jsx/MyProfile/MyProfile.js
+++ src/jsx/MyProfile/MyProfile.js
@@ -1,15 +1,15 @@
 import "./MyProfile.css";
 import "../components/Theme.css";
 import ProfileFramework from "../components/ProfileFramework";
-import {useContext} from "react";
-import AuthContext from "../AuthContext";
+//import {useContext} from "react";
+//import AuthContext from "../AuthContext";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function MyProfile() {
-    let {user} = useContext(AuthContext)
+    //let {user} = useContext(AuthContext)
     //let role;
-    if (user.is_business)
+    //if (user.is_business)
         //role = <p>Role: Business</p>
         return (
             <div className="page-content">
