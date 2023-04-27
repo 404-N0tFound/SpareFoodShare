adf067be302e4ebd1ee4bfbb1877b90dd0d4b113
elp20xt <xtan30@sheffield.ac.uk>
2023-04-27 11:27:04 +0100

    new changes to my code

--- src/jsx/MyProfile/MyProfile.js
+++ src/jsx/MyProfile/MyProfile.js
@@ -1,16 +1,10 @@
 import "./MyProfile.css";
 import "../components/Theme.css";
 import ProfileFramework from "../components/ProfileFramework";
-//import {useContext} from "react";                         //I keep the code here for some potential later changes:)
-//import AuthContext from "../AuthContext";                 //I keep the code here for some potential later changes:)
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 function MyProfile() {
-    //let {user} = useContext(AuthContext)                  //I keep the code here for some potential later changes:)
-    //let role;
-    //if (user.is_business)
-        //role = <p>Role: Business</p>
         return (
             <div className="page-content">
                 <Navbar/>
@@ -21,28 +15,25 @@ function MyProfile() {
                             <ul>
                                 <h2>Personal Details</h2>
                                 <li className="li1">
-                                    <label htmlFor="username">Name：</label>
-                                    <input type="text" className="inp" name="username" id="username" placeholder=""/>
+                                    <label htmlFor="username">User&nbsp;&nbsp;Name：</label>
+                                    <input type="text" className="inp" id="username" placeholder=""/>
                                 </li>
                                 <li>
                                     <label htmlFor="phone_number">Phone&nbsp;&nbsp;number：</label>
-                                    <input type="text" className="inp" name="username" id="phone_number" placeholder="+44"/>
+                                    <input type="text" className="inp"  id="phone_number" placeholder="+44"/>
                                 </li>
-                                <li>
-                                    <label htmlFor="email">Email：</label>
-                                    <input type="email" className="inp" name="username" id="email" placeholder=""/>
-                                </li>
+
                                 <li>
                                     <label htmlFor="password">Password：</label>
-                                    <input type="password" className="inp" name="username" id="password" placeholder=""/>
+                                    <input type="password" className="inp" id="password" placeholder=""/>
                                 </li>
                                 <li>
                                     <label htmlFor="location">Location：</label>
-                                    <input type="text" className="inp" name="username" id="location" placeholder=""/>
+                                    <input type="text" className="inp" id="location" placeholder=""/>
                                 </li>
                                 <li>
                                     <label htmlFor="description">My&nbsp;&nbsp;Introduction：</label>
-                                    <input type="text" className="inp" name="username" id="description" placeholder="Some words to introduce yourself!" />
+                                    <input type="text" className="inp" id="description" placeholder="Some words to introduce yourself!" />
                                 </li>
                             </ul>
                             <div>
